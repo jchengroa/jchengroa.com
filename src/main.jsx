af257496f3b6c5d83f230b6a70eb95a04802b744
John Carlo Cheng Roa <chengroaj2@gmail.com>
2026-04-28 20:45:26 +0800

    [QUICK FIX] Fixed Version Number

--- src/main.jsx
+++ src/main.jsx
@@ -63,7 +63,7 @@ function App() {
                             <b>Domain & Legal Information</b>
                         </Link>
                     </p>
-                    <p className="text-gray-500 text-sm">Version 0.3.2 | Last Updated: April 28, 2026</p>
+                    <p className="text-gray-500 text-sm">Version 0.3.4 | Last Updated: April 28, 2026</p>
                 </div>
             </div>
         </BrowserRouter>
